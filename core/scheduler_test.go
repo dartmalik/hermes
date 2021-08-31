af11cff41dcb0b4c5b9e48270469f2afa5017072
dartmalik <dartmalik@yahoo.com>
2021-08-31 11:48:50 +0500

    * fixed ping-pong test to use requests rather than async send

--- core/scheduler_test.go
+++ core/scheduler_test.go
@@ -1,13 +1,17 @@
 package hermes
 
 import (
+	"errors"
 	"fmt"
 	"testing"
 	"time"
 )
 
-type SendPing struct {
+type SendPingRequest struct {
 	to ReceiverID
+}
+type SendPingResponse struct {
+	err error
 }
 
 type Ping struct{}
@@ -20,26 +24,28 @@ type PongActor struct {
 
 func (a *PongActor) receive(ctx *Context, msg Message) {
 	switch msg.Payload().(type) {
-	case *SendPing:
-		a.onSendPing(ctx, msg)
+	case *SendPingRequest:
+		err := a.onSendPing(ctx, msg)
+		ctx.Reply(msg, &SendPingResponse{err: err})
 
 	case *Ping:
 		a.onPing(ctx, msg)
 	}
 }
 
-func (a *PongActor) onSendPing(ctx *Context, msg Message) {
-	sp := msg.Payload().(*SendPing)
+func (a *PongActor) onSendPing(ctx *Context, msg Message) error {
+	sp := msg.Payload().(*SendPingRequest)
 
 	reply, err := ctx.RequestWithTimeout(sp.to, &Ping{}, 1500*time.Millisecond)
 	if err != nil {
-		fmt.Println("request timed out")
-		return
+		return err
 	}
 
-	if _, ok := reply.Payload().(*Pong); ok {
-		fmt.Printf("received pong\n")
+	if _, ok := reply.Payload().(*Pong); !ok {
+		return errors.New("invalid_reply_type")
 	}
+
+	return nil
 }
 
 func (a *PongActor) onPing(ctx *Context, msg Message) {
@@ -48,6 +54,10 @@ func (a *PongActor) onPing(ctx *Context, msg Message) {
 	fmt.Printf("received ping: %d\n", a.count)
 
 	ctx.Reply(msg, &Pong{})
+}
+
+type Tester interface {
+	Fatalf(format string, args ...interface{})
 }
 
 func TestBasic(t *testing.T) {
@@ -76,24 +86,7 @@ func TestMessagePassing(t *testing.T) {
 		t.Fatalf("new actor system failed with error: %s\n", err.Error())
 	}
 
-	a1 := ReceiverID("t1")
-	a2 := ReceiverID("t2")
-
-	err = net.Join(a1)
-	if err != nil {
-		t.Fatalf("actor registeration failed with error: %s\n", err.Error())
-	}
-
-	err = net.Join(a2)
-	if err != nil {
-		t.Fatalf("actor registeration failed with error: %s\n", err.Error())
-	}
-
-	for i := 0; i < 100; i++ {
-		net.Send(a1, a1, &SendPing{to: a2})
-	}
-
-	time.Sleep(1000 * time.Millisecond)
+	passMessage(t, net, 0, 1000)
 }
 
 func BenchmarkMessagePassing(b *testing.B) {
@@ -107,30 +100,38 @@ func BenchmarkMessagePassing(b *testing.B) {
 
 	fmt.Printf("running test with runs: %d\n", b.N)
 	for ri := 0; ri < b.N; ri++ {
-		passMessage(b, net, ri)
+		passMessage(b, net, ri, 1)
 	}
 }
 
-func passMessage(b *testing.B, net *Hermes, run int) {
+func passMessage(t Tester, net *Hermes, run int, iter int) {
 	a1 := ReceiverID(fmt.Sprintf("t1-%d", run))
 	a2 := ReceiverID(fmt.Sprintf("t2-%d", run))
 
 	err := net.Join(a1)
 	if err != nil {
-		b.Fatalf("actor registeration failed with error: %s\n", err.Error())
+		t.Fatalf("actor registeration failed with error: %s\n", err.Error())
 	}
 
 	err = net.Join(a2)
 	if err != nil {
-		b.Fatalf("actor registeration failed with error: %s\n", err.Error())
+		t.Fatalf("actor registeration failed with error: %s\n", err.Error())
 	}
 
-	for i := 0; i < 1; i++ {
-		net.Send(a1, a1, &SendPing{to: a2})
+	for i := 0; i < iter; i++ {
+		msg, err := net.RequestWithTimeout(a1, &SendPingRequest{to: a2}, 100*time.Millisecond)
+		if err != nil {
+			t.Fatalf("failed to send ping request: %s\n", err.Error())
+		}
+
+		r := msg.Payload().(*SendPingResponse)
+		if r.err != nil {
+			t.Fatalf("failed to send ping request: %s\n", err.Error())
+		}
 	}
 
-	//net.Leave(a1)
-	//net.Leave(a2)
+	net.Leave(a1)
+	net.Leave(a2)
 }
 
 func TestReceiverMembership(t *testing.T) {
